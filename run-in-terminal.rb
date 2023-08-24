d825883884a79b71176f188dc9348190dbab0a30
Will B Chang <will.b.chang@gmail.com>
2023-08-24 11:42:46 +0800

    use updated applescript

--- run-in-terminal.rb
+++ run-in-terminal.rb
@@ -26,4 +26,26 @@ query = ARGV[0]
 terminal = 'Terminal'
 
 
-`osascript -e 'tell app "#{terminal}" to do script "#{get_script(query)}" activate'`
+`
+osascript <<EOF
+-- Get the title of the Terminal window
+-- Please enable the Active process name
+-- Settings → Profiles → Window → Active process name
+tell application "#{terminal}"
+	activate
+	set frontWindow to front window
+	set windowTitle to name of frontWindow
+
+	-- Check if the window title does not end with "-zsh" or "▸ zsh"(figterm)
+	if not (windowTitle ends with "-zsh" or windowTitle ends with "▸ zsh") then
+		-- Create a new tab
+		tell application "System Events" to keystroke "t" using command down
+	end if
+
+	-- Select current tab and run shell script
+	set theTab to selected tab in first window
+	do script "#{get_script(query)}" in theTab
+end tell
+EOF
+`
+
